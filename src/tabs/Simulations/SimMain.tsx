6ea7560a4d1e08b7c8ec75f01cbc73c8c61e1ec6
Alik <alovsat.2000.com@gmail.com>
2025-11-25 17:21:32 +0300

    Fix Accordion path

--- src/tabs/Simulations/SimMain.tsx
+++ src/tabs/Simulations/SimMain.tsx
@@ -3,7 +3,7 @@ import {
   AccordionItem,
   AccordionTrigger,
   AccordionContent
-} from "../../../components/ui/Accordion";
+} from "../../../../components/ui/Accordion";
 
 
 export default function SimMain() {
