02a4d974ac964dc3fbe03d474f0c57dbcac4d417
Alik <alovsat.2000.com@gmail.com>
2025-11-26 19:06:43 +0300

    no com

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,4 +1,4 @@
-export function SectionTitle({ title }: { title: string }) {
+export defalut function SectionTitle({ title }: { title: string }) {
   return (
     <h2 className="text-3xl font-bold text-gray-900 mb-6">
       {title}
